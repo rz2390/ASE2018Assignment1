b951c18635f59ab6208e75b7338d1fb64325c67f
ruimin zhao <rz2390@columbia.edu>
2018-09-16 19:40:07 -0400

    Change that helloWorld.java file again on my local machine

--- code/helloWorld.java
+++ code/helloWorld.java
@@ -1,5 +1,6 @@
 public class helloWorld {
 	public static void main(String[] args) {
 		System.out.println("After Edition made directly using web browser");
+		System.out.println("I made some more modification using my local machine editor");
 	}
 }
